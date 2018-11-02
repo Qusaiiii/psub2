744f9c9ece079106749a89efd2ea45c3f936417b
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-11-02 13:54:44 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!';
+
 client.on('warn', console.warn);
 
 client.on('error', console.error);
@@ -13,15 +14,49 @@ client.on('disconnect', () => console.log('PROBOT credits miner had disconnected
 
 client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
 
-
 function timerFunc() {
     client.on('message', msg => {
-        client.channels.find('spam').send(Math.random().toString(36).substring(7))
+      let spam = msg.guild.channels.find('name', 'spam');
+        spam.send(Math.random().toString(36).substring(7))
 
 
     });
 }
 
+client.on('message', message => {
+
+  if (message.author.bot) return;
+
+  if (!message.content.startsWith(prefix)) return;
+
+
+
+  let command = message.content.split(" ")[0];
+
+  command = command.slice(prefix.length);
+
+
+
+let args = message.content.split(" ").slice(1);
+
+if(!args) return message.channel.send('${prefix}say <words>');	
+
+
+
+if (command == "say") {
+
+if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply("*لا تملك الصلاحيات المطلوبه**");
+
+
+
+message.channel.send(args.join("  "))
+
+    message.delete();
+
+  }
+
+});
 
 var timer = setTimeout(timerFunc, 1000);
+
 client.login(process.env.BOT_TOKEN); 
